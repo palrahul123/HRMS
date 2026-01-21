3f884b793da0fed382c3ba910252d82c405221fd
Rahul123 <pr@unstoppablesoftwares.com>
2026-01-21 09:01:06 +0530

    f

--- Infrastructure.Persistence/EntityConfiguration/UserEntityConfiguration.cs
+++ Infrastructure.Persistence/EntityConfiguration/UserEntityConfiguration.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Infrastructure.Persistence.EntityConfiguration
 {
-    public class UserEntityConfiguration  : IEntityTypeConfiguration<>
+    public class UserEntityConfiguration : IEntityTypeConfiguration<User>
     {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
